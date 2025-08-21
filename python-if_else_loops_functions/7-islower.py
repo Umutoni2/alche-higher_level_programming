9194e16c0f78b224b8c6cabd93034c5abcb5b788
Rutaganira <s.umutonir@alustudent.com>
2025-08-21 10:22:37 -0700

    Added a new file named 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
-    if c >= 97 and c <= 123
-        return (chr(c))
+    if ord(c) >= 97 and ord(c) <= 123:
+     return ord(c)
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
