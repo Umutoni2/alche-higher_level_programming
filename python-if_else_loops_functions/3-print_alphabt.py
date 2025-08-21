656ccb527e2631d93341d98423bf969aaaa31736
Rutaganira <s.umutonir@alustudent.com>
2025-08-21 04:15:02 -0700

    Added a new file called 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(ard("a"),ard("z"):
-    if chr(i)   == "e" or chr("q"):
-      continue
+for i in range(ard("a"), ard("z"):
+    if chr(i)  == "e" or chr("q"):
+        continue
     print("{}".format(char(i))
-             
