3218253a41477dda0d7b32aa8d004671e8ab63ed
Rutaganira <s.umutonir@alustudent.com>
2025-08-21 11:32:03 -0700

    Added 8-uppercase.pyfile

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uppercase(s):
     for i in s:
-        print("{}".format(chr(ord(i) - 32) if 'a' <= i <= 'z' else i
-        ), end="")
+        print("{}".format(chr(ord(i) - 32) if 'a' <= i <= 'z' else i), end="")
     print()
