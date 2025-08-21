bf981a00ea22d9dae44b67530ac25e5fc12a2123
Rutaganira <s.umutonir@alustudent.com>
2025-08-21 09:45:42 -0700

    Added file named 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for a in range(i + 1, 10):
         if i == 8 and a == 9:
-            print("{}{}".format(i, a)
+            print("{}{}".format(i, a))
         else:
             print("{}{}".format(i, a), end=", ")
