f6a150355f4eceb4f237e4a3dc9b70ed81fb9129
Rutaganira <s.umutonir@alustudent.com>
2025-08-21 06:35:06 -0700

    Added new file named 5-print_comb2.py

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for a in range(i + 1, 10):
+        if i == 8 and a == 9:
+            print(f"{i}{a}", end="")
+        else:
+            print(f"{i}{a}", end=", ")
