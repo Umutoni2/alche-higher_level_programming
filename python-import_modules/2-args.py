673983876a6dd82e0cd427eb290baa11a30e6723
Rutaganira <s.umutonir@alustudent.com>
2025-08-24 14:14:33 +0400

    importing modules

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if (len(argv) - 1) == 0:
+        print("0 arguments.")
+    else:
+        if (len(argv) - 1) == 1 and (len(argv) - 1) != 0:
+            print("{} argument:".format((len(argv) - 1)))
+        elif (len(argv) - 1) > 1:
+            print("{} arguments:".format((len(argv)-1)))
+        for i in range(len(argv) - 1):
+            print("{}:".format(i+1), argv[i+1])
