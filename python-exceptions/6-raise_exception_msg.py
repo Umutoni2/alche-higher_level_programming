ac8f051c9f4339b4060f5ca02a59d51a76cc8bdd
Rutaganira <s.umutonir@alustudent.com>
2025-09-06 09:01:31 -0700

    Added file named 6-raise_exception_msg.py ,after i found it with issues then I modified it

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
+    raise NameError(message)
     try:
-        raise NameError(message)
+        raise NameError("C is fun")
     except Exception as e:
         print("Python is cool")
