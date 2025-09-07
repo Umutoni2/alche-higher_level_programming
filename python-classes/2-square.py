78e231bdc9997c532de397a09cb9ac15c689a1f2
Rutaganira <s.umutonir@alustudent.com>
2025-09-07 05:31:39 -0700

    Added file named 2-square.py to python-classes folder

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,10 +4,11 @@
 
 class Square:
     """Represents a square."""
+
     def __init__(self, size=0):
         """Initialize a new Square."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.size = size
-        try:
-            size > 0
-        except (TypeError,ValueError):
-            print(" size must be >= 0")
