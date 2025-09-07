bf06b64e08033f82b63cf75000dc6c1379358cea
Rutaganira <s.umutonir@alustudent.com>
2025-09-07 04:46:59 -0700

    Added new file named 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+        """Initialize a new Square."""
+        self.__size = size    
+
+    
