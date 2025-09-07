de9eba554f9566bdd69b1c779b42c464717cb92d
Rutaganira <s.umutonir@alustudent.com>
2025-09-07 05:54:41 -0700

    fixed 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-This module defines the Square class.
-"""
+"""This module defines the Square class."""
 
 
 class Square:
-    """Defines a square with a private size attribute, a getter, and a setter."""
+    """Defines a square with a private size and position attribute."""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initializes a new Square instance."""
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -18,16 +17,37 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """
-        Sets the size of the square with validation."""
+        """Sets the size of the square with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """Retrieves the position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position of the square with validation."""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(i, int) and i >= 0 for i in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
-        """
-        Calculates and returns the current area of the square.
-        """
+        """Calculates and returns the current area of the square."""
         return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
