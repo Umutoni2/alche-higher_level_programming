140e09aed9239cf475ee387d0b919c3cdaf488c8
Umutoni2 <s.umutonir@alustudent.com>
2025-09-07 17:58:06 +0400

    Removed whitespace in blank line 49:1 4-rectangle.py

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -44,7 +44,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        
+            
         rectangle_str = ""
         for i in range(self.__height):
             rectangle_str += "#" * self.__width
