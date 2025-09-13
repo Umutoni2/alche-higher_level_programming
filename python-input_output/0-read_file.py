ccfc579d688020b0d8feb3960a565b3ad430f680
Umutoni2 <s.umutonir@alustudent.com>
2025-09-13 20:52:34 +0400

    Updated 0-read_file.py(documented module)

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" This is for Reading a file"""
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its contents to stdout."""
     with open(filename, encoding="utf-8") as f:
