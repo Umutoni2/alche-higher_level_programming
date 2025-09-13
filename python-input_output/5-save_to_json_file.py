eadfeaa084e063cc429a9752c79a306d6e89e9f6
Umutoni2 <s.umutonir@alustudent.com>
2025-09-13 21:13:52 +0400

    Added a missing line and removed unnecessary white spaces

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,9 +4,8 @@
 
 import json
 
-def save_to_json_file(my_obj, filename):
 
-    
+def save_to_json_file(my_obj, filename):
     """Writes a Python object to a text file using its JSON representation."""
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
