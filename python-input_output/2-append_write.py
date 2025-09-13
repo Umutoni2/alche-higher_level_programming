ce3f58323123c83603cde1826f760dfbaf256228
Umutoni2 <s.umutonir@alustudent.com>
2025-09-13 20:55:13 +0400

    Updated 2-append_write.py by documenting module and adding 2 blank lines

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""a function that appends a string at the end of a text file (UTF8) and returns the number of characters added"""
+
+
 def append_write(filename="", text=""):
     """Appends a string to a UTF-8 text file and returns the number of characters added."""
     with open(filename, "a", encoding="utf-8") as f:
